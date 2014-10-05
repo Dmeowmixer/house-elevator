aec9862dd365eef8552bf82b232aa109d5cfad88
Ranson <Ransonikuo@gmail.com>
2014-10-04 15:38:59 -1000

    set values to floor numbers?

--- elevator.js
+++ elevator.js
@@ -2,5 +2,32 @@
 'use strict';
 
 module.exports = {
+	goto: function(currentFloor,'destination'){
+		//codehere
+	var travel = "";
+	if (currentFloor >= 0){
+		currentFloor+travel
+	}
+	}
 
+
+	destination+=travel;
+		return travel;
+		//var/number//;
+	}
 };
+goto(1,3);
+/*
+function goTo(currentFloor,destination){
+var travel = "";
+
+if (currentFloor >= 0 ){
+    currentFloor+travel;
+} 
+
+
+
+travel += currentFloor + destination;
+return travel;
+}
+goTo(3,1);*/
