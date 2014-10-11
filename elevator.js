4fcec3ab2ce9b5e2dd4db47f9a0661b82bc7cc1b
Ranson <Ransonikuo@gmail.com>
2014-10-10 17:05:45 -1000

    FINISHED FNASMKDAJFN

--- elevator.js
+++ elevator.js
@@ -2,32 +2,23 @@
 'use strict';
 
 module.exports = {
-	goto: function(currentFloor,'destination'){
-		//codehere
-	var travel = "";
-	if (currentFloor >= 0){
-		currentFloor+travel
-	}
-	}
-
-
-	destination+=travel;
-		return travel;
-		//var/number//;
-	}
+	goto : function(currentFloor,destination){
+        var travel;
+        if (typeof currentFloor != 'number' || typeof destination != 'string'){
+            return 0;
+        }
+        if (currentFloor < 4 && destination < 4) {
+            travel = destination - currentFloor;
+            return travel;
+        }
+        else if(currentFloor < 3 || destination < 3){
+            travel = 0;
+            currentFloor = 0;
+            return travel;
+        }
+        else if (currentFloor>destination) { 
+            travel = currentFloor - destination;
+            return travel;
+        }
+    }
 };
-goto(1,3);
-/*
-function goTo(currentFloor,destination){
-var travel = "";
-
-if (currentFloor >= 0 ){
-    currentFloor+travel;
-} 
-
-
-
-travel += currentFloor + destination;
-return travel;
-}
-goTo(3,1);*/
